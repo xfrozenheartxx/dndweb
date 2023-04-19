c0156e7b26bc7b7617e64963d3225dae7f6fafaa
frutzi <78870673+xfrozenheartxx@users.noreply.github.com>
2023-04-18 19:40:01 -0700

    base updated

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://xfrozeneartxx.github.io/dndweb/'
+  base: 'https://xfrozeheartxx.github.io/dndweb/'
 })
